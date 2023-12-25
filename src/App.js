53688c7828fda25e711cc5aeefcd1fffe970d7aa
Mansovic <mansouraldundur@gmail.com>
2023-12-26 02:56:31 +0300

    add prject files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
-function App() {
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import RandomRecipePage from './pages/RandomRecipePage';
+import AllRecipesPage from './pages/AllRecipesPage';
+import AllRecipesPageGraphQL from './pages/AllRecipesPageGraphQL';
+import RandomRecipePageGraphQL from './pages/RandomRecipePageGraphQL';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/signup">Signup</Link></li>
+          <li><Link to="/random-recipe">Random Recipe</Link></li>
+          <li><Link to="/all-recipes">All Recipes</Link></li>
+          <li><Link to="/all-recipes-graphql">ALL Recipe GraphQL</Link></li>
+          <li><Link to="/random-recipe-graphql">Random GraphQL</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/random-recipe" element={<RandomRecipePage />} />
+        <Route path="/all-recipes" element={<AllRecipesPage />} />
+        <Route path="/all-recipes-graphql" element={<AllRecipesPageGraphQL/>} />
+        <Route path="/random-recipe-graphql" element={<RandomRecipePageGraphQL/>} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
